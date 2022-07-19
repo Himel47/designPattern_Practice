a81c0f5612a79a3c8b36b323170d9a5b51b60790
Md. Shahriar Kabir Himel <52915408+Himel47@users.noreply.github.com>
2022-07-19 09:47:10 +0600

    nothing

--- #Behavioral Pattern/src/Template_Pattern/Main_Tamplate.java
+++ #Behavioral Pattern/src/Template_Pattern/Main_Tamplate.java
@@ -3,6 +3,10 @@ package Template_Pattern;
 public class Main_Tamplate {
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+
         Class c = Class.forName(args[0]);
         Game game = (Game) c.newInstance();
         game.play();
